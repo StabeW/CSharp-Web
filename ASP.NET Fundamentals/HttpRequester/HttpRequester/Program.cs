4b4ea35a76124a36585b2f0dbdd94261ef72bfa6
deluxc <136242540+StabeW@users.noreply.github.com>
2024-03-29 11:28:37 +0200

    Update Program.cs
    Adding Cookies and Session in HTTP response

--- ASP.NET Fundamentals/HttpRequester/HttpRequester/Program.cs
+++ ASP.NET Fundamentals/HttpRequester/HttpRequester/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 
-public class Program
+public partial class Program
 {
+    public static Dictionary<string, int> sessionStore = [];
     public static async Task Main(string[] args)
     {
         TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 8000);
         tcpListener.Start();
+
         while (true)
         {
             TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
@@ -23,21 +26,44 @@ public class Program
         using NetworkStream networkStream = tcpClient.GetStream();
         byte[] requestBytes = new byte[10000];
 
-        int bytesRead = await networkStream.ReadAsync(requestBytes, 0, requestBytes.Length);
+        int bytesRead = await networkStream.ReadAsync(requestBytes);
         string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
 
-        string responseText = @"<form action='/Account/Login' method= 'post'> 
-<input type=data name='data' />
-<input type=text name='username' />
-<input type=password name='password' />
-<input type=submit value='login' />
-</form>" + "<h1>" + DateTime.UtcNow + "</h1>";
-        Thread.Sleep(1000);
+        var sid = MyRegex().Match(request).Value?.Replace("sid=", string.Empty);
+
+        var newSid = Guid.NewGuid().ToString();
+
+        var count = 0;
+
+        if (sessionStore.ContainsKey(sid))
+        {
+            sessionStore[sid]++;
+            count = sessionStore[sid];
+        }
+        else
+        {
+            sid = null;
+            sessionStore[newSid] = 1;
+            count = 1;
+        }
+
+        string responseText =
+            @"<form action='/Account/Login' method= 'post'> 
+            <input type=data name='data' />
+            <input type=text name='username' />
+            <input type=password name='password' />
+            <input type=submit value='login' />
+            </form>" + "<h1>" + count + "</h1>" + 
+            "<h1>" + DateTime.UtcNow + "</h1>";
+        
         string response = "HTTP/1.0 200 OK" + NewLine +
                           "Server: SoftUniServer/1.0" + NewLine +
                           "Content-Type: text/html" + NewLine +
-                          // "Location: https://google.com" + NewLine +
-                          // "Content-Disposition: attachment; filename=stabew.html" + NewLine +
+                        (string.IsNullOrWhiteSpace(sid) ?
+                        ("Set-Cookie: sid=" + newSid + NewLine)
+                        : string.Empty) +
+                          "Set-Cookie: name=StabeW; path=/; Max-Age=3200; HttpOnly; " + NewLine +
+                          "Set-Cookie: lang=en; path=/; HttpOnly; Max-Age=3200" + NewLine +
                           "Content-Lenght: " + responseText.Length + NewLine +
                           NewLine +
                           responseText;
@@ -46,4 +72,7 @@ public class Program
         Console.WriteLine(request);
         Console.WriteLine(new string('=', 60));
     }
+
+    [GeneratedRegex(@"sid=[^\n]*\n")]
+    private static partial Regex MyRegex();
 }
